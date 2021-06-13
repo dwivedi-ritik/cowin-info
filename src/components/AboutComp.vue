38e4ab069d674a088908206b8566a1a3f0ce06b4
ritik <dwi.ritik20@gmail.com>
2021-06-13 18:28:18 +0530

    added new tab

--- src/components/AboutComp.vue
+++ src/components/AboutComp.vue
@@ -4,12 +4,12 @@
     <div class="head-detail-in">
       <p id="head-text"> Data of vaccination are not realtime as gov choosed to not show it due to people were using
         script to book an appointment. Gov is only showing the limited one day data . They are not showing the future data.
-        and by the way if you ever need any snippet code of this site feel free to get it on my <a href="https://github.com/dwivedi-ritik/cowin-info" id="link">github repository</a>.
+        and by the way if you ever need any snippet code of this site feel free to get it on my <a href="https://github.com/dwivedi-ritik/cowin-info" id="link" target="_blank">github repository</a>.
       </p>
       <p id="head-text">All the APIs are open source and i have add them as a link my home route.If you loved my site in any way 
         a star on github will be very appriciated 🤓.
       </p>
-      <p id="head-text">Source of data is <a href="https://dashboard.cowin.gov.in/" id="link">Cowin-portal </a>, 
+      <p id="head-text">Source of data is <a href="https://dashboard.cowin.gov.in/" id="link" target="_blank">Cowin-portal </a>, 
           click at Cowin text for more information
           wll i know site is incomplete in many , i made it just as pratice project 
           details at the side of arrow shows the todays increased data 
